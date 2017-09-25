f137aad9e086c85b84262f6e5920bd3c5fbbd608
dannyclark77 <dannyclark77@gmail.com>
2017-09-25 09:10:44 -0400

    Set up players table and seed initial data
    Create and migrate players table and seed data from 2008 team

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+players = Player.create([
+  { name: 'Dave Hoel', year: 2008 },
+  { name: 'Teddy Browar-Jarus', year: 2008 },
+  { name: 'Ryan Todd', year: 2008 },
+  { name: 'Josh Mullen', year: 2008 },
+  { name: 'Kevin Albert', year: 2008 },
+  { name: 'Mike Zalisk', year: 2008 },
+  { name: 'Jasper Hoitsma', year: 2008 },
+  { name: 'Will Neff', year: 2008 },
+  { name: 'Fortunat Mueller', year: 2008 },
+  { name: 'Jacob Goldstein', year: 2008 },
+  { name: 'Doug Moore', year: 2008 },
+  { name: 'Dan Forseter', year: 2008 },
+  { name: 'Adam Sigelman', year: 2008 },
+  { name: 'Dan Patisteas', year: 2008 },
+  { name: 'Sam Rosenthal', year: 2008 },
+  { name: 'Matt Holzer', year: 2008 },
+  { name: 'Colin Mahoney', year: 2008 },
+  { name: 'Kurt Gibson', year: 2008 },
+  { name: 'Jeff Graham', year: 2008 },
+  { name: 'Trey Katzenbach', year: 2008 },
+  { name: 'George Stubbs', year: 2008 },
+  { name: 'Josh McCarthy', year: 2008 },
+  { name: 'Ben Faust', year: 2008 },
+  { name: 'Steve Kolthammer', year: 2008 },
+  { name: 'Brent Anderson', year: 2008 },
+  { name: 'Paul Batten', year: 2008 },
+  { name: 'Danny Clark', year: 2008 }
+])
