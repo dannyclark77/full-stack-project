887b214b033f92d0d15f288c132182ef694d876d
dannyclark77 <dannyclark77@gmail.com>
2017-09-25 09:52:50 -0400

    Create get player and get players curl scripts
    Create and test get request curl scripts for players

---
+++ scripts/players/get-player.sh
@@ -0,0 +1,2 @@
+curl --include --request GET "http://localhost:4741/players/${ID}" \
+  --header "Content-Type: application/json"
---
+++ scripts/players/get-players.sh
@@ -0,0 +1,2 @@
+curl --include --request GET "http://localhost:4741/players" \
+  --header "Content-Type: application/json"
