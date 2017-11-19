d0f227831b67caa6e77a3104feb0bd2ccfea862b
dannyclark77 <dannyclark77@gmail.com>
2017-11-19 17:23:30 -0500

    Add column 'team_name' to table teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925134127) do
+ActiveRecord::Schema.define(version: 20171119220239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170925134127) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "team_name"
     t.index ["player_id"], name: "index_teams_on_player_id"
     t.index ["user_id"], name: "index_teams_on_user_id"
   end
