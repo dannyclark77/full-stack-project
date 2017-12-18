dbec16cfb19ac870082d1b8e428702f8cc7aeaf2
dannyclark77 <dannyclark77@gmail.com>
2017-12-17 21:59:49 -0500

    Switch validate uniqueness of for player id
    - Switch from user id to team id to allow single user to create
    multiple teams with same player across those teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,6 @@ class Team < ApplicationRecord
   belongs_to :player
   belongs_to :user
 
-  validates_uniqueness_of :player_id, scope: :user_id
+  validates_uniqueness_of :player_id, scope: :team_name
   validates :team_name, presence: true, allow_blank: false
 end
