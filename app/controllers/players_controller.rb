390df0d97802acf0bcb2df16aab325b6f907cfb0
dannyclark77 <dannyclark77@gmail.com>
2018-01-11 11:00:48 -0500

    Add query to players get request
    - Query by year of request

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,7 +3,7 @@ class PlayersController < OpenReadController
 
   # GET /players
   def index
-    @players = Player.all
+    @players = Player.where(year: params[:year])
 
     render json: @players
   end
